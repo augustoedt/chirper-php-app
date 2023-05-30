b0d07ed86af580c5c04b79698954da122a029162
Augusto Eduardo <augustoedt@gmail.com>
2023-05-30 12:33:16 -0300

    create has-many relation user chirp

--- database/migrations/2023_05_30_034939_create_chirps_table.php
+++ database/migrations/2023_05_30_034939_create_chirps_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('chirps', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('message');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
         });
     }
 
